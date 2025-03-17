2a2c7796f6b9500ac708ffccc6ae0a0a67cba553
valurisho <153689011+valurisho@users.noreply.github.com>
2025-03-17 08:39:46 -0600

    Add extra files to database

---
+++ backend/CutlerApp/CutlerApp/Migrations/20250316035119_Inittial.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CutlerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inittial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Callings",
+                columns: table => new
+                {
+                    CallingId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CallingName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Callings", x => x.CallingId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wards",
+                columns: table => new
+                {
+                    WardId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    WardName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wards", x => x.WardId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    MiddleName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Birthday = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    FheGroupId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CallingId = table.Column<int>(type: "INTEGER", nullable: false),
+                    WardId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberId);
+                    table.ForeignKey(
+                        name: "FK_Members_Callings_CallingId",
+                        column: x => x.CallingId,
+                        principalTable: "Callings",
+                        principalColumn: "CallingId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Members_Wards_WardId",
+                        column: x => x.WardId,
+                        principalTable: "Wards",
+                        principalColumn: "WardId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CalendarTasks",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MemberId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ChurchMemberMemberId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TaskDescription = table.Column<string>(type: "TEXT", nullable: false),
+                    TaskDate = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarTasks", x => x.TaskId);
+                    table.ForeignKey(
+                        name: "FK_CalendarTasks_Members_ChurchMemberMemberId",
+                        column: x => x.ChurchMemberMemberId,
+                        principalTable: "Members",
+                        principalColumn: "MemberId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalendarTasks_ChurchMemberMemberId",
+                table: "CalendarTasks",
+                column: "ChurchMemberMemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_CallingId",
+                table: "Members",
+                column: "CallingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_WardId",
+                table: "Members",
+                column: "WardId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalendarTasks");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Callings");
+
+            migrationBuilder.DropTable(
+                name: "Wards");
+        }
+    }
+}
