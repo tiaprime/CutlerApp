a0bc7ce475766a5c7c788205a752d76e6a7e94ff
amymmcd <amymargaret22@gmail.com>
2025-03-20 19:43:08 -0600

    FHE css updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css'
-import Login from './Components/Login'
+import './App.css';
+import Login from './Components/Login';
 import FHEGroups from './pages/FHEGroups';
 
 function App() {
