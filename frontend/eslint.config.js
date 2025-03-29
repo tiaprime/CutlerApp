c060b006ede968caee6e85ba82e4b8724d0c40f8
tiaprime <tiaprime@byu.edu>
2025-03-28 19:19:15 -0600

    I think I just ruined this branch

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,13 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import prettier from 'eslint-config-prettier'
-import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended], eslintConfigPrettier,
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -18,7 +16,6 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'prettier': prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -26,7 +23,6 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'prettier/prettier': 'error'
     },
   },
 )
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,35 @@
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './pages/Login'
-import WardDashboard from './Components/WardDashboard';
-import FHEGroups from './pages/FHEGroups';
-import ToolsPage from './Components/ToolsPage';
-import NewsCard from './Components/NewsCard';
-import EditProfilePage from './pages/EditProfilePage';
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-
     <>
-      <Router>
-
-        <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/WardDashboard" element={<WardDashboard/>} />
-          <Route path="/Tools" element={<ToolsPage/>} />
-          <Route path="/FHEGroups" element={<FHEGroups/>} />
-          <Route path="/News" element={<NewsCard title={''} location={''}/>} />
-          <Route path="/Profile" element={<EditProfilePage/>} />
-        </Routes>
-      </Router>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
