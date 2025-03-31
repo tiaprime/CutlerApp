225b7d2a9032fbeee4f6fca75d5eff86fcf1b4e2
valurisho <153689011+valurisho@users.noreply.github.com>
2025-03-30 22:18:24 -0600

    Taking out status bar

--- frontend/src/pages/EditProfilePage.tsx
+++ frontend/src/pages/EditProfilePage.tsx
@@ -1,27 +1,25 @@
-"use client";
-import React from "react";
-import StatusBar from "../Components/StatusBar";
-import ProfileHeader from "../Components/ProfileHeader";
-import ProfilePicture from "../Components/ProfilePicture";
-import ProfileForm from "../Components/ProfileForm";
-import BottomNavigation from "../Components/BottomNavigation";
-import BackButton from "../Components/BackButton";
+'use client';
+import React from 'react';
+import ProfileHeader from '../Components/ProfileHeader';
+import ProfilePicture from '../Components/ProfilePicture';
+import ProfileForm from '../Components/ProfileForm';
+import BottomNavigation from '../Components/BottomNavigation';
+import BackButton from '../Components/BackButton';
 
 const EditProfilePage: React.FC = () => {
-    return (
-        <main className="relative flex flex-col min-h-screen w-full bg-[#F4F4F4] before:absolute before:top-0 before:left-0 before:w-full before:h-[35%] before:bg-[#006184] before:rounded-b-[50px]">
-            <StatusBar />
-            <div className="relative w-full px-4">
-                <BackButton />
-                <ProfileHeader title="Edit Profile" />
-            </div>
-            <section className="flex flex-col items-center w-full max-w-lg mx-auto px-4 mt-6">
-                <ProfilePicture imageUrl='/image.png' />
-                <ProfileForm />
-            </section>
-            <BottomNavigation />
-        </main>
-    );
+  return (
+    <main className="relative flex flex-col min-h-screen w-full bg-[#F4F4F4] before:absolute before:top-0 before:left-0 before:w-full before:h-[35%] before:bg-[#006184] before:rounded-b-[50px]">
+      <div className="relative w-full px-4">
+        <BackButton />
+        <ProfileHeader title="Edit Profile" />
+      </div>
+      <section className="flex flex-col items-center w-full max-w-lg mx-auto px-4 mt-6">
+        <ProfilePicture imageUrl="/image.png" />
+        <ProfileForm />
+      </section>
+      <BottomNavigation />
+    </main>
+  );
 };
 
 export default EditProfilePage;
