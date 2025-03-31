80ce40e48143e31f316544caacf6867c956a2b87
amymmcd <amymargaret22@gmail.com>
2025-03-30 20:59:53 -0600

    back button functionality on fhe groups page

--- frontend/src/Components/BackButton.tsx
+++ frontend/src/Components/BackButton.tsx
@@ -1,9 +1,12 @@
-import styles from "../css/BackButton.module.css";
+import styles from '../css/BackButton.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const BackButton = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.backButtonContainer}>
-      <button className={styles.backButton}>
+      <button className={styles.backButton} onClick={() => navigate(-1)}>
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/e7611222beb85d1ad4a0b369a1f7e4ca0f01750e"
           alt="Back"
