ed1839eca1348a28d81868d3613858b65b5080e7
valurisho <153689011+valurisho@users.noreply.github.com>
2025-03-30 22:12:17 -0600

    Add routes and fix css

--- frontend/src/Components/ActionButtons.tsx
+++ frontend/src/Components/ActionButtons.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './WardDashboard.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const ActionButtons: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.actionButtonsContainer}>
       <button className={styles.actionButton}>Calendar</button>
-      <button className={styles.actionButton}>Tools</button>
+      <button
+        className={styles.actionButton}
+        onClick={() => navigate('/Tools')}
+      >
+        Tools
+      </button>
       <button className={styles.actionButton}>Ministering</button>
     </div>
   );
